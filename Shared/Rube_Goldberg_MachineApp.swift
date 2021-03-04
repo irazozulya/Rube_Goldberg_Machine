8d078ec761e405d79a942683448f620fb8ce8bf3
irazozulya <45012463+irazozulya@users.noreply.github.com>
2021-03-04 16:08:07 +0200

    Initial Commit

---
+++ Shared/Rube_Goldberg_MachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  Rube_Goldberg_MachineApp.swift
+//  Shared
+//
+//  Created by Ira Zozulya on 04.03.2021.
+//
+
+import SwiftUI
+
+@main
+struct Rube_Goldberg_MachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
